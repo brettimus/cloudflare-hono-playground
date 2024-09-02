5195e79c72a9d86ace6c6211f6cd531d9e06099d
Brett Beutell <brbeut@gmail.com>
2024-09-02 16:19:16 +0200

    Flesh out r2 a bit

--- src/routes/r2.ts
+++ src/routes/r2.ts
@@ -15,12 +15,21 @@ app.get("/list", async (c) => {
 // Get a specific object from the R2 bucket
 app.get("/get/:key", async (c) => {
   const object = await c.env.MY_BUCKET.get(c.req.param("key"));
-  // TODO - return the correct headers
-  return c.json(object);
+
+  if (!object) {
+    return c.json({ message: "Object not found" }, 404);
+  }
+
+  const responseHeaders = mapR2HttpMetadataToHeaders(object.httpMetadata);
+
+  return c.body(object.body, {
+    headers: responseHeaders,
+  });
 });
 
 /**
  * Upload an object to the R2 bucket
+ *
  * @param key - The key of the object to upload
  * @returns - The uploaded object
  */
@@ -36,8 +45,49 @@ app.post("/put/:key", async (c) => {
  * @returns - 204 No Content
  */
 app.delete("/delete/:key", async (c) => {
-  await c.env.MY_BUCKET.delete(c.req.param("key"));
-  return c.body(null, 204);
+  try {
+    await c.env.MY_BUCKET.delete(c.req.param("key"));
+    // HACK - Fiberplane Studio can't handle 204s right now
+    return c.text("", 200);
+  } catch (error) {
+    return c.json({ message: "Failed to delete object", error }, 500);
+  }
 });
 
 export default app;
+
+/**
+ * Helper function that returns the headers for a given R2 object
+ *
+ * @param metadata - The R2 HTTP metadata
+ * @returns - The headers
+ */
+function mapR2HttpMetadataToHeaders(metadata?: R2HTTPMetadata): Headers {
+  const headers = new Headers();
+
+  if (!metadata) {
+    return headers;
+  }
+
+  if (metadata.contentType) {
+    headers.set("Content-Type", metadata.contentType);
+  }
+  if (metadata.contentLanguage) {
+    headers.set("Content-Language", metadata.contentLanguage);
+  }
+  if (metadata.contentDisposition) {
+    headers.set("Content-Disposition", metadata.contentDisposition);
+  }
+  if (metadata.contentEncoding) {
+    headers.set("Content-Encoding", metadata.contentEncoding);
+  }
+  if (metadata.cacheControl) {
+    headers.set("Cache-Control", metadata.cacheControl);
+  }
+  if (metadata.cacheExpiry) {
+    headers.set("Cache-Expiry", metadata.cacheExpiry.toUTCString());
+  }
+
+  return headers;
+}
+
